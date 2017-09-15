13526c50c3f6ad3c9cdf49cc57993d64e76f4fe7
Lzolla <31737389+Lzolla@users.noreply.github.com>
2017-09-15 12:10:13 +0200

    edit calibration.bat
    Suprression de la calibration auto

--- interface_adb/calibration.bat
+++ interface_adb/calibration.bat
@@ -4,22 +4,3 @@ start vysor.exe
 TIMEOUT /T 2 /NOBREAK
 adb shell "am start -a android.media.action.STILL_IMAGE_CAMERA -W"
 
-:: à régler potentiellement, attention valeurs adaptées à l'écran du smartphone client
-
-
-:: ouvre les options
-adb shell "input tap 1282 2181"
-timeout /T 1 /NOBREAK > NUL
-:: choisit le mode pro
-adb shell "input tap 1286 921"
-timeout /T 1 /NOBREAK > NUL
-:: ouvre les réglages
-adb shell "input tap 1285 1961"
-:: gère les couleurs
-adb shell "input tap 712 649"
-:: gère l'iso
-adb shell "input tap 429 1273"
-:: gère la durée d'exposition
-adb shell "input tap 1064 1027"
-:: gère la luminosité
-adb shell "input tap 217 1615"
